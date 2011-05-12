780e71971f310141f6a99998b8e064076ad8ca98
Eskat0n <muyou.prj@gmail.com>
2011-05-12 23:21:06 +0600

    implemented filters settings property

--- muyou.twitter.user.js
+++ muyou.twitter.user.js
@@ -5,13 +5,38 @@
 
 var Settings = function () {
     this.autoshow = localStorage['muyou.autoshow'] != 'false';
+    this.filters = [];
+
+    var filters = localStorage['muyou.filters'];
+    if (filters) this.filters = eval(filters);
 };
 Settings.prototype = {
+    _join: function (array) {
+        var aggregate = '';
+        for (var i = 0; i < array.length; i++) {
+            var item = array[i];
+            if (i != 0) aggregate += ',';
+            aggregate += '"' + item.replace('"', '\\"') + '"';
+        }
+        return aggregate;
+    },
     set: function (key, val) {
         localStorage['muyou.' + key] = val.toString();
         this[key] = val;
+    },
+    setFilters: function (filters) {
+        this.filters = filters;
+
+        var serializedFilters = '[';
+        for (var i = 0; i < filters.length; i++) {
+            var filter = filters[i];
+            serializedFilters += '{"nick":"' + filter.nick + '","words":[' + this._join(filter.words) + ']}';
+        }
+        serializedFilters += ']';
+
+        localStorage['muyou.filters'] = serializedFilters;
     }
-}
+};
 
 var settings = new Settings();
 var initialized = false;
@@ -27,7 +52,7 @@ var toggleAutoshow = function () {
     return false;
 };
 
-var initialize = function (tabsPanel) {
+var initialize = function (tabsPanel, globalNav) {
     if (initialized) return;
 
     var autoshowLink = document.createElement('A');
@@ -52,10 +77,15 @@ var applyClick = function (element) {
     return element.dispatchEvent(event);
 };
 
+var filter = function (element) {
+    //'stream-item'
+};
+
 var onNodeInserted = function (event) {
     if (!initialized)
         if (event.srcElement.querySelectorAll) {
             var tabsPanel = event.srcElement.querySelectorAll('ul.stream-tabs')[0];
+
             if (tabsPanel)
                 initialize(tabsPanel);
         }
@@ -64,7 +94,28 @@ var onNodeInserted = function (event) {
 
     if (event.srcElement.id && event.srcElement.id == 'new-tweets-bar')
         applyClick(event.srcElement);
+
+    filter(event.srcElement);
 };
+
+// init global nav panel settings
+var showSettings = function () {
+
+};
+
+var globalNav = document.querySelectorAll('#global-nav ul')[0];
+
+var settingsLink = document.createElement('A');
+settingsLink.setAttribute('href', '#');
+settingsLink.innerText = 'Settings';
+settingsLink.onclick = showSettings;
+
+var settingsItem = document.createElement('LI');
+settingsItem.className = 'global-nav-settings';
+settingsItem.appendChild(settingsLink);
+
+globalNav.appendChild(settingsItem);
+// end init
 
 document.addEventListener("DOMNodeInserted", onNodeInserted);
 
